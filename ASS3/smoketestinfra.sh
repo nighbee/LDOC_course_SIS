c168b30ac08174828b9402210861a049c2d85541
Almaz <95701510+nighbee@users.noreply.github.com>
2025-10-24 09:44:42 +0500

    Refactor smoketestinfra.sh for improved readability

--- ASS3/smoketestinfra.sh
+++ ASS3/smoketestinfra.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
-echo "infra smoke test: " 
+echo "infra smoke test: "
 
-docker --version &&  echo "Docker: Pass" || echo "Docker: Fail" 
-docker compose version && echo "compose: pass" || echo "compose: fail" 
-psql --version && echo "postgresql: up" || echo "postgres: fail" 
-go version && echo "go: pass" || echo "go: fail" 
-node --version && echo "node: pass" || echo "node: fail" 
-npm --version && echo "npm: pass" || echo "npm: fail" 
-sudo ufw status | head -5 & echo "ufw: pass" || echo "ufw: fail" 
-go mod tidy -v && echo "go mod: pass" || echo "go mod: fail" 
+docker --version &&  echo "Docker: Pass" || echo "Docker: Fail"
+docker compose version && echo "compose: pass" || echo "compose: fail"
+psql --version && echo "postgresql: up" || echo "postgres: fail"
+go version && echo "go: pass" || echo "go: fail"
+node --version && echo "node: pass" || echo "node: fail"
+npm --version && echo "npm: pass" || echo "npm: fail"
+sudo ufw status | head -5 && echo "ufw: pass" || echo "ufw: fail"
 
+cd /home/ESG/backend || { echo "backend folder not found"; exit 1; }
+go mod tidy -v && echo "go mod: pass" || echo "go mod: fail"
+cd - > /dev/null
